cbb503fc474c2a8a9da4c0357b87cc8774fac433
Diegoher <diegohrerr92@gmail.com>
2020-07-14 22:32:51 -0500

    home page and hamburger menu added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  speed: number;
+  counter: number;
+  text: string;
+  // welcomeTextArray: any;
+  constructor() {
+    // this.welcomeTextArray = {
+    //   text1: 
+    // };
+    this.text = 'typewriter';
+    console.log('text=>', this.text.length);
+    this.counter = 0;
+    this.speed = 50;
   }
 
+  ngOnInit(): void {
+    const hamburger = document.querySelector('.hamburger-container');
+    const navLinks = document.querySelector('.nav-links');
+    const links = document.querySelectorAll('.nav-links li');
+
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('open');
+      console.log('navLinks=>', navLinks);
+      links.forEach((link) => {
+        console.log('link=>', link);
+        link.classList.toggle('fade');
+      });
+    });
+    // const numbers = interval(100);
+    // const takeFourNumbers = numbers.pipe(take(this.text.length));
+    // takeFourNumbers.subscribe((x) => {
+    //   console.log('Next: ', x);
+    //   document.getElementById('demo').innerHTML += this.text.charAt(x);
+    // });
+  }
 }
